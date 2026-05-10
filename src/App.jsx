eda04df9ee62d4005f14e4bc9f40345bece0ef13
modernProteus <nick.proteus@gmail.com>
2026-05-10 09:34:50 -0500

    Set React Router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Toaster } from "@/components/ui/toaster"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import PageNotFound from './lib/PageNotFound'
-import Home from './pages/Home'
-
+import { Toaster } from "@/components/ui/toaster";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PageNotFound from "./lib/PageNotFound";
+import Home from "./pages/Home";
+const routerBaseName = import.meta.env.BASE_URL.replace(/\/$/, "");
 function App() {
   return (
-    <Router>
+    <Router basename={routerBaseName}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Toaster />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
